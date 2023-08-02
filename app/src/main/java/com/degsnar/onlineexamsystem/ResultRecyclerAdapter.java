aebde35b06d1dca1bc16b4038698e9ddfea5c4d5
vit-ndpm <137673140+vit-ndpm@users.noreply.github.com>
2023-08-03 00:29:49 +0530

    ResultDetail function working with bugs

--- app/src/main/java/com/degsnar/onlineexamsystem/ResultRecyclerAdapter.java
+++ app/src/main/java/com/degsnar/onlineexamsystem/ResultRecyclerAdapter.java
@@ -1,5 +1,8 @@
 package com.degsnar.onlineexamsystem;
 
+import static com.degsnar.onlineexamsystem.R.drawable.tv_correct_border;
+import static com.degsnar.onlineexamsystem.R.drawable.tv_wrong_border;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +12,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -19,61 +21,127 @@ public class ResultRecyclerAdapter extends RecyclerView.Adapter<ResultRecyclerAd
     ArrayList<Question> questionArrayList;
     ArrayList<ResultModel> resultModelArrayList;
 
-    public ResultRecyclerAdapter(Context context, ArrayList<Question> questionArrayList, ArrayList<ResultModel> resultModelArrayList)
-    {
+    public ResultRecyclerAdapter(Context context, ArrayList<Question> questionArrayList, ArrayList<ResultModel> resultModelArrayList) {
         this.context = context;
         this.questionArrayList = questionArrayList;
         this.resultModelArrayList = resultModelArrayList;
-        Toast.makeText(context, String.valueOf(questionArrayList.size()), Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "Questions: " + questionArrayList.size(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "Response: " + resultModelArrayList.size(), Toast.LENGTH_SHORT).show();
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.result_row,parent,false);
-        ViewHolder viewHolder=new ViewHolder(view);
+        View view = LayoutInflater.from(context).inflate(R.layout.result_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.q_number.setText(questionArrayList.get(position).question_no);
+        holder.q_number.setText(String.valueOf(questionArrayList.get(position).question_no));
         holder.question.setText(questionArrayList.get(position).question);
-        holder.opt1.setText(questionArrayList.get(position).option1);
-        holder.opt2.setText(questionArrayList.get(position).option2);
-        holder.opt3.setText(questionArrayList.get(position).option3);
-        holder.opt4.setText(questionArrayList.get(position).option4);
-        holder.description.setText(questionArrayList.get(position).description);
+        holder.opt1.setText(String.valueOf(questionArrayList.get(position).option1));
+        holder.opt2.setText(String.valueOf(questionArrayList.get(position).option2));
+        holder.opt3.setText(String.valueOf(questionArrayList.get(position).option3));
+        holder.opt4.setText(String.valueOf(questionArrayList.get(position).option4));
+        holder.correctOption.setText(String.valueOf(questionArrayList.get(position).correct_option));
+        holder.descripition.setText(questionArrayList.get(position).description);
+        int questionId = questionArrayList.get(position).id;
+        if (checkIfSelectedCorrectOption(questionId) <4) {
+            int selectedOption = checkIfSelectedCorrectOption(questionId);
+            switch (selectedOption) {
+                case 1:
+                    holder.img1.setVisibility(View.VISIBLE);
+                    holder.img1.setImageResource(R.drawable.correct);
+                    holder.opt1.setBackgroundResource(tv_correct_border);
+                    break;
+                case 2:
+                    holder.img2.setVisibility(View.VISIBLE);
+                    holder.img2.setImageResource(R.drawable.correct);
+                    holder.opt2.setBackgroundResource(tv_correct_border);
+                    break;
+                case 3:
+                    holder.img3.setVisibility(View.VISIBLE);
+                    holder.img3.setImageResource(R.drawable.correct);
+                    holder.opt3.setBackgroundResource(tv_correct_border);
+                    break;
+                case 4:
+                    holder.img4.setVisibility(View.VISIBLE);
+                    holder.img4.setImageResource(R.drawable.correct);
+                    holder.opt4.setBackgroundResource(tv_correct_border);
+                    break;
+                default:
+                    break;
+            }
+        }else {
+            int selectedOption = checkIfSelectedCorrectOption(questionId);
+            switch (selectedOption) {
+                case 101:
+                    holder.img1.setVisibility(View.VISIBLE);
+                    holder.img1.setImageResource(R.drawable.baseline_close_24);
+                    holder.opt1.setBackgroundResource(tv_wrong_border);
+                    break;
+                case 102:
+                    holder.img2.setVisibility(View.VISIBLE);
+                    holder.img2.setImageResource(R.drawable.baseline_close_24);
+                    holder.opt2.setBackgroundResource(tv_wrong_border);
+                    break;
+                case 103:
+                    holder.img3.setVisibility(View.VISIBLE);
+                    holder.img3.setImageResource(R.drawable.baseline_close_24);
+                    holder.opt3.setBackgroundResource(tv_wrong_border);
+                    break;
+                case 104:
+                    holder.img4.setVisibility(View.VISIBLE);
+                    holder.img4.setImageResource(R.drawable.baseline_close_24);
+                    holder.opt4.setBackgroundResource(tv_wrong_border);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+    }
+
+    private int checkIfSelectedCorrectOption(int questionId) {
+        for (int i = 0; i < resultModelArrayList.size(); i++) {
+            ResultModel resultModel = resultModelArrayList.get(i);
+            if (resultModel.question_id == questionId && resultModel.correct_option == resultModel.selected_option) {
+                return resultModel.correct_option;
+            } else if(resultModel.question_id == questionId && resultModel.correct_option != resultModel.selected_option){
+                return 100+resultModel.selected_option;
+            }
+
+        }
+        return 10000;
     }
 
     @Override
     public int getItemCount() {
-
         return questionArrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ConstraintLayout ql1,ql2,ql3,ql4;
-        TextView q_number,question,opt1,opt2,opt3,opt4,description;
-        ImageView imgOpt1,imgOpt2,imgOpt3,imgOpt4;
+        TextView q_number, question, opt1, opt2, opt3, opt4, descripition, correctOption;
+        ImageView img1, img2, img3, img4;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            ql1=itemView.findViewById(R.id.constraintLayoutQ1);
-            ql2=itemView.findViewById(R.id.constraintLayoutQ2);
-            ql3=itemView.findViewById(R.id.constraintLayoutQ3);
-            ql4=itemView.findViewById(R.id.constraintLayoutQ4);
-            q_number=itemView.findViewById(R.id.q_number);
-            question=itemView.findViewById(R.id.question);
-            opt1=itemView.findViewById(R.id.textOpt1);
-            opt2=itemView.findViewById(R.id.textOpt2);
-            opt3=itemView.findViewById(R.id.textOpt3);
-            opt4=itemView.findViewById(R.id.textOpt4);
-            description=itemView.findViewById(R.id.description);
-            imgOpt1=itemView.findViewById(R.id.imgOpt1);
-            imgOpt2=itemView.findViewById(R.id.imgOpt2);
-            imgOpt3=itemView.findViewById(R.id.imgOpt3);
-            imgOpt4=itemView.findViewById(R.id.imgOpt4);
-
+            q_number = itemView.findViewById(R.id.q_numberTv);
+            question = itemView.findViewById(R.id.questionTv);
+            opt1 = itemView.findViewById(R.id.opt1Tv);
+            opt2 = itemView.findViewById(R.id.opt2Tv);
+            opt3 = itemView.findViewById(R.id.opt3Tv);
+            opt4 = itemView.findViewById(R.id.opt4Tv);
+            correctOption = itemView.findViewById(R.id.correctOptionTv);
+            descripition = itemView.findViewById(R.id.descriptionTv);
+            img1 = itemView.findViewById(R.id.img1);
+            img2 = itemView.findViewById(R.id.img2);
+            img3 = itemView.findViewById(R.id.img3);
+            img4 = itemView.findViewById(R.id.img4);
         }
     }
 }
